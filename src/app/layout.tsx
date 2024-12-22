81926e1c8eb111f8a4df3c98fe043974f27c066b
namphuong <phuongnampham7823@gmail.com>
2024-12-22 14:39:56 +0700

    fix: server rendered HTML didn't match the client properties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,10 @@
 'use client'
 
 import {store} from '@src/core/redux/store'
-import localFont from 'next/font/local'
 import React from 'react'
 import {Provider} from 'react-redux'
 import './globals.css'
 import {ThemeProvider} from './shared/ThemeProvider'
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
