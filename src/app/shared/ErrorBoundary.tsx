8117aef9a0450d75c6c714c86f4b4b5d0ddef28c
nhatphanhk <114910072+nhatphanhk@users.noreply.github.com>
2024-12-30 00:12:54 +0700

    SCRUM-52 <message>

--- src/app/shared/ErrorBoundary.tsx
+++ src/app/shared/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {Button} from '@src/components/ui/button'
 import React from 'react'
 
 export default class ErrorBoundary extends React.Component<
@@ -26,10 +27,16 @@ export default class ErrorBoundary extends React.Component<
   render() {
     if (this.state.hasError) {
       return (
-        <div>
-          <h1>Something went wrong!</h1>
-          <p>{this.state.error?.message}</p>
-          <button onClick={this.resetError}>Retry</button>
+        <div className='bg-white min-h-screen flex items-center justify-center p-4'>
+          <div className='text-center space-y-4'>
+            <h1 className='text-2xl font-bold text-red-600'>Something went wrong!</h1>
+            <p className='text-gray-600'>
+              {this.state.error?.message || 'An unexpected error occurred'}
+            </p>
+            <Button onClick={this.resetError} variant='destructive' className='w-6/12'>
+              Retry
+            </Button>
+          </div>
         </div>
       )
     }
