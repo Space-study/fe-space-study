eccee351ff65b13fb34605550ff38491efd62c87
namphuong <phuongnampham7823@gmail.com>
2025-01-12 15:03:52 +0700

    fix: resolve config package

--- src/core/pages/customer/RoomPageClient.tsx
+++ src/core/pages/customer/RoomPageClient.tsx
@@ -2,7 +2,7 @@
 
 import Header from '@/core/components/customer/room/Header'
 import Scenes from '@/core/components/customer/room/Scenes'
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 const RoomPageClient: React.FC = () => {
   // const [isMobileView, setIsMobileView] = useState(false)
