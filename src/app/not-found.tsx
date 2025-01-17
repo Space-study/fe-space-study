af8d200b19b48c8b39f4c66c6a234771926151e2
nhatphanhk <114910072+nhatphanhk@users.noreply.github.com>
2025-01-17 15:01:41 +0700

    merge main

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
-import {Button} from '@src/components/ui/button'
+import {Button} from '@src/core/components/ui/button'
+import {ArrowLeft} from 'lucide-react'
 import Link from 'next/link'
 
 export default function NotFound() {
@@ -16,23 +17,8 @@ export default function NotFound() {
       </p>
 
       <Link href='/' className='mt-8'>
-        <Button
-          variant='secondary'
-          className='text-white px-8 py-2 rounded-md flex items-center gap-2'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='w-5 h-5'>
-            <path d='m12 19-7-7 7-7' />
-            <path d='M19 12H5' />
-          </svg>
+        <Button className='text-white px-8 py-2 rounded-md flex items-center gap-2'>
+          <ArrowLeft />
           BACK TO HOME
         </Button>
       </Link>
